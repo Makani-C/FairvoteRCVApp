f17c13b2fd9a70307282ae46cafbc9a4660ae346
Makani-C <61200430+Makani-C@users.noreply.github.com>
2025-05-10 10:33:51 -0700

    some fly

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM python:3.9
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt psycopg2-binary
 
 COPY ./app /app
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD uvicorn main:app --host 0.0.0.0 --port 8000
--- app/config.py
+++ app/config.py
@@ -3,7 +3,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/rcv")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+
 
 
 settings = Settings()
+if settings.DATABASE_URL.startswith('postgres:'):
+    settings.DATABASE_URL = settings.DATABASE_URL.replace('postgres:', 'postgresql:', 1)
